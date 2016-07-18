46b2cc8be8780a5a5d2573bd5d33481915a63fa1
franciscosucre <frank91frank@gmail.com>
2016-07-18 15:40:04 -0400

    (FJSG) Practica 3 lista

---
+++ Actividad_0/practica_4.R
@@ -0,0 +1,21 @@
+# Practica_4 --------------------------------------------------------------
+
+# Dado los datos USJudgeRatings
+# 1 Entienda la estructura de los datos, ?USJudgeRatings muestra los
+# detalles de cada columna
+# 2 Estime el juez con mayor y/o menor score rowSums, min, max
+# 3 Estime el promedio grupal (summary) y los promedios de cada prueba
+# 4 Ordene los jueces según su score sort, matrix, rownames
+
+
+# Parte I -----------------------------------------------------------------
+
+
+# Parte II ----------------------------------------------------------------
+
+
+# Parte III ---------------------------------------------------------------
+
+
+# Parte IV ----------------------------------------------------------------
+
