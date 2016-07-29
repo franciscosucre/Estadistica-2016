aedc6179bc67a70ff2d7fa5a3e963c3f66e80f65
franciscosucre <frank91frank@gmail.com>
2016-07-29 15:18:20 -0400

    (FJSG) Ya se lee el archivo de texto

---
+++ Lab2/lab2.R
@@ -0,0 +1,12 @@
+# Laboratorio 2
+# Estadistica Para Ingenieros
+# Francisco Sucre 10-10717
+# José Cipagauta 05-38040
+
+library(MASS) ## loading package MASS
+library(stats4) ## loading package stats4
+
+data <- scan("personas.txt", what=numeric(), sep="")
+
+#1
+
