3194cb448e354c710f1519b6e3f579af1bfbc829
franciscosucre <frank91frank@gmail.com>
2016-07-29 23:17:47 -0400

    (FJSG) PDF Final lab 2

--- Lab2/lab2.R
+++ Lab2/lab2.R
@@ -4,7 +4,6 @@
 # José Cipagauta 05-38040
 
 library(MASS) ## Cargando libreria MASS para usar fitdistr
-#library(stats4) ## No usamos esta
 
 data <- scan("personas.txt")
 
@@ -16,8 +15,11 @@ summary(estimator) ##Summary muestra cosas extrañas y locas aqui
 # comprar en la provedurı́a estudiantil antes de que se venda el décimo dorito?
 lambda = estimator$estimate
 esperado = lambda
+esperado
 varianza = lambda
+varianza
 
 # Si una hora especı́fica se atienden 50 personas, ¿cuál es la probabilidad de
 # que se vendan más de 10 doritos en esa hora?
 probabilidad = ppois(50, lambda)
+probabilidad
