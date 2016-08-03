2ad3e21e41e260d57873435c131996c7b864ca9e
franciscosucre <frank91frank@gmail.com>
2016-08-03 16:10:20 -0400

    (FJSG) Lab 3 virtualmente terminado

--- Lab3/Lab3.R
+++ Lab3/Lab3.R
@@ -3,6 +3,9 @@
 # Francisco Sucre 10-10717
 # José Cipagauta 05-38040
 
+# Se tienen las notas del primer examen de tres secciones de un mismo curso. 
+# Los examenes son evaluados sobre 100 puntos.
+
 # Cargamos la data
 Notas11_12 <- read.table("Notas11-12.txt", header = T,fill=NA)
 Notas12_13 <- read.table("Notas12-13.txt", header = T,fill=NA)
@@ -66,18 +69,62 @@ qqline(S9)
 # Se aproxima a la distribucion normal asi que si se puede tomar como una
 # distribucion normal
 
-# Respuesta: 
-
 # 2. Calcule intervalos de confianza del 97 % para la media de la nota de cada sección. 
 # ¿Qué consideraciones se deben tomar en cuenta para construir estos intervalos?
 
+# Consideraciones: SI los datos se comportan como una distribucion normal, puede que el
+# intervalo no sea el mas correcto o que los resultados no nos presenten informacion 
+# util
+
+S5_tTest = t.test(S5,conf.level = 0.97)
+S6_tTest = t.test(S6,conf.level = 0.97)
+S13_tTest = t.test(S13,conf.level = 0.97)
+S3_tTest = t.test(S3,conf.level = 0.97)
+S4_tTest = t.test(S4,conf.level = 0.97)
+S9_tTest = t.test(S9,conf.level = 0.97)
 
 
 # 3. ¿Qué conclusiones puede sacar a partir de los intervalos de confianza de sus variables? 
 # En particular, ¿cómo comparariría los resultados entre secciones?
+S5_tTest
+S6_tTest
+S13_tTest
+S3_tTest
+S4_tTest
+S9_tTest
 
+# Comparaciones
+t.test(S5,S6,conf.level = 0.97)
 
+# Podemos ver que la media de S5 en general deberia ser mucho mayor que la de S6
+# Pero podemos decir que las medias pueden ser iguales
+
+t.test(S5,S13,conf.level = 0.97)
+
+# Podemos ver que la media de S5 siempre es mayor que la de S13
+
+t.test(S6,S13,conf.level = 0.97)
+
+# Podemos ver que la media de S6 en general deberia ser mucho mayor que la de S13
+# Pero podemos decir que las medias pueden ser iguales
+
+t.test(S3,S4,conf.level = 0.97)
+
+# Podemos ver que la media de S3 en general deberia ser mucho mayor que la de S4
+# Pero podemos decir que las medias pueden ser iguales
+
+t.test(S3,S9,conf.level = 0.97)
+
+# Podemos ver que la media de S3 siempre es mayor que la de S9
+
+t.test(S4,S9,conf.level = 0.97)
+
+# Podemos ver que la media de S4 casi siempre es mayor que la de S9 pero nunca es igual
 
 # 4. Si realizan la comparación de los intervalos de confianza entre años, 
 # ¿cómo los comparariría? ¿Qué puede concluir al hacer la comparación?
 
+# Comparacion De Varianzas por años
+var.test(c(S5,S6,S13),c(S3,S4,S9),conf.level = 0.97)
+# Podemos concluir que las medias que las varianzas de ambos años se pueden tomar como 
+# iguales ya que el intervalo de confianza incluye al 1
