7316984a69ec09c48f0b17bc59280a1a9b0eba27
MrZyrus <kaiser705@gmail.com>
2016-08-03 20:47:12 -0400

    Update Lab3.R

--- Lab3/Lab3.R
+++ Lab3/Lab3.R
@@ -7,8 +7,8 @@
 # Los examenes son evaluados sobre 100 puntos.
 
 # Cargamos la data
-Notas11_12 <- read.table("Notas11-12.txt", header = T,fill=NA)
-Notas12_13 <- read.table("Notas12-13.txt", header = T,fill=NA)
+Notas11_12 <- read.table("Notas11-12.txt", header = T, fill=NA)
+Notas12_13 <- read.table("Notas12-13.txt", header = T, fill=NA)
 attach(Notas11_12)
 attach(Notas12_13)
 
@@ -17,51 +17,45 @@ attach(Notas12_13)
 
 # Grafico Notas11_12
 # S5
-par(mfrow=c(1,1))
 hist(S5)
 boxplot(S5)
 qqnorm(S5)
 qqline(S5)
-# NO es normal ni se aproxima a serlo, excepto en secciones muy pequeñas
+# No es normal ni se aproxima a serlo, excepto en secciones muy pequeñas
 
 # S6
-par(mfrow=c(1,1))
 hist(S6)
 boxplot(S6)
 qqnorm(S6)
 qqline(S6)
-# Tiene unas secciones que no se acercan a estar distribuidas de forma normal, pero en 
-# pero en la seccion del medio se comporta de manera muy parecida a una distribucion normal
+# Tiene unas secciones que no se acercan a estar distribuidas de forma normal, pero 
+# en la seccion del medio se comporta de manera muy parecida a una distribucion normal
 
 # S13
-par(mfrow=c(1,1))
 hist(S13)
 boxplot(S13)
 qqnorm(S13)
 qqline(S13)
-# Se aproxima a la distribucion normal, excepto en las colas, pero si se puede tomar como una
-# distribucion normal
+# Se aproxima a la distribucion normal, excepto en las colas, aun asi se podria tomar como una
+# distribucion normal, pero las conclusiones obtenidas al asumir esto podrian ser erradas
 
 # S3
-par(mfrow=c(1,1))
 hist(S3)
 boxplot(S3)
 qqnorm(S3)
 qqline(S3)
-# Se aproxima a la distribucion normal asi que si se puede tomar como una
+# Se aproxima bastante a la distribucion normal asi que si se puede tomar como una
 # distribucion normal
 
 # S4
-par(mfrow=c(1,1))
 hist(S4)
 boxplot(S4)
 qqnorm(S4)
 qqline(S4)
-# Se aproxima a la distribucion normal, excepto en las colas, pero si se puede tomar como una
+# Se aproxima a la distribucion normal, excepto en las colas con datos atipicos, no se recomienda tomar como una
 # distribucion normal
 
 # S9
-par(mfrow=c(1,1))
 hist(S9)
 boxplot(S9)
 qqnorm(S9)
@@ -69,12 +63,12 @@ qqline(S9)
 # Se aproxima a la distribucion normal asi que si se puede tomar como una
 # distribucion normal
 
-# 2. Calcule intervalos de confianza del 97 % para la media de la nota de cada sección. 
+# 2. Calcule intervalos de confianza del 97% para la media de la nota de cada sección. 
 # ¿Qué consideraciones se deben tomar en cuenta para construir estos intervalos?
 
-# Consideraciones: SI los datos se comportan como una distribucion normal, puede que el
+# Consideraciones: Que los datos pertenezcan a una distribucion normal, puede que el
 # intervalo no sea el mas correcto o que los resultados no nos presenten informacion 
-# util
+# util si la distribucion no es normal
 
 S5_tTest = t.test(S5,conf.level = 0.97)
 S6_tTest = t.test(S6,conf.level = 0.97)
@@ -83,9 +77,6 @@ S3_tTest = t.test(S3,conf.level = 0.97)
 S4_tTest = t.test(S4,conf.level = 0.97)
 S9_tTest = t.test(S9,conf.level = 0.97)
 
-
-# 3. ¿Qué conclusiones puede sacar a partir de los intervalos de confianza de sus variables? 
-# En particular, ¿cómo comparariría los resultados entre secciones?
 S5_tTest
 S6_tTest
 S13_tTest
@@ -93,33 +84,43 @@ S3_tTest
 S4_tTest
 S9_tTest
 
+# Concluciones interesantes es que considerando que el examen es evaluado sobre 100 puntos
+# y en el mejor de los casos, con un 97% de confiabilidad el mayor promedio es 28.64 
+# entre todas las secciones, lo cual indica un promedio terriblemente bajo
+
+# 3. ¿Qué conclusiones puede sacar a partir de los intervalos de confianza de sus variables? 
+# En particular, ¿cómo comparariría los resultados entre secciones?
+
+# Las comparaciones se harian con intervalos de confianza de la diferencia entre medias
+
 # Comparaciones
 t.test(S5,S6,conf.level = 0.97)
 
-# Podemos ver que la media de S5 en general deberia ser mucho mayor que la de S6
-# Pero podemos decir que las medias pueden ser iguales
+# Podemos ver que la media de S6 podria ser mayor que la de S5
+# Pero al incluir el 0, podemos decir que las medias son relativamente iguales
 
 t.test(S5,S13,conf.level = 0.97)
 
-# Podemos ver que la media de S5 siempre es mayor que la de S13
+# Podemos ver que la media de S13 siempre es mayor que la de S5, por al menos 2 puntos
 
 t.test(S6,S13,conf.level = 0.97)
 
-# Podemos ver que la media de S6 en general deberia ser mucho mayor que la de S13
-# Pero podemos decir que las medias pueden ser iguales
+# Podemos ver que la media de S13 podria ser mayor que la de S6
+# Pero al incluir el 0, podemos decir que las medias son relativamente iguales
 
 t.test(S3,S4,conf.level = 0.97)
 
-# Podemos ver que la media de S3 en general deberia ser mucho mayor que la de S4
-# Pero podemos decir que las medias pueden ser iguales
+# Podemos ver que la media de S4 podria ser mayor que la de S3
+# Pero al incluir el 0, podemos decir que las medias son relativamente iguales
 
 t.test(S3,S9,conf.level = 0.97)
 
-# Podemos ver que la media de S3 siempre es mayor que la de S9
+# Podemos ver que la media de S9 siempre es mayor que la de S3, por al menos 3 puntos
 
 t.test(S4,S9,conf.level = 0.97)
 
-# Podemos ver que la media de S4 casi siempre es mayor que la de S9 pero nunca es igual
+# Podemos ver que la media de S9 es generalmente mayor que la de S4 pero aun asi
+# incluye el 0, por lo cual podrian ser iguales
 
 # 4. Si realizan la comparación de los intervalos de confianza entre años, 
 # ¿cómo los comparariría? ¿Qué puede concluir al hacer la comparación?
